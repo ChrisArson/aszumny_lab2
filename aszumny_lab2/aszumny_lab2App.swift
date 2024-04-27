bece8cc9cb8854e3221355626fb79c04b1522833
x <x>
2024-04-27 14:26:44 +0200

    Initial Commit

---
+++ aszumny_lab2/aszumny_lab2App.swift
@@ -0,0 +1,17 @@
+//
+//  aszumny_lab2App.swift
+//  aszumny_lab2
+//
+//  Created by student on 27/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct aszumny_lab2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
